850b3d0f505338a518d69b1417709d847f28ecd3
theo <fntelnovfn@gmail.com>
2020-12-09 16:15:08 +0300

    Working with the net requests

--- authdialog.cpp
+++ authdialog.cpp
@@ -2,6 +2,12 @@
 #include "ui_authdialog.h"
 #include <QDebug>
 #include "notificationdialog.h"
+#include <QNetworkReply>
+#include <QJsonValue>
+#include <QJsonValueRef>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
 AuthDialog::AuthDialog(QWidget *parent) :
     QDialog(parent),
@@ -9,17 +15,17 @@ AuthDialog::AuthDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     this->ui->passwordRepeatInput->hide();
+    this->manager = new QNetworkAccessManager(this);
 }
 
 AuthDialog::~AuthDialog()
 {
     delete ui;
 }
-
 void AuthDialog::on_authButton_clicked()
 {
     if(this->is_signup) {
-        QString nickname = this->ui->authNicknameInput->toPlainText();
+        QString nickname = this->ui->authNicknameInput->text();
         QString password = this->ui->passwordInput->text();
         QString repeatedPassword = this->ui->passwordRepeatInput->text();
         if (nickname.length() < 4) {
@@ -33,20 +39,42 @@ void AuthDialog::on_authButton_clicked()
                 break;
             }
         }
+        if(repeatedPassword != password) {
+            pushNotification("Passwords do not match");
+            return;
+        }
         if(containsDigit && password.length() >= 8){
-            if(repeatedPassword != password) {
-                pushNotification("Passwords do not match");
-                return;
-            }
+            QNetworkRequest request(QUrl(baseUrl + "api/signup/"));
+            request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+            QByteArray postData;
+            postData.append("nickname=" + nickname + "$");
+            postData.append("password=" + password);
+            connect(this->manager, SIGNAL(finished(QNetworkReply*)), this,
+                SLOT(signupReplyFinished(QNetworkReply*)));
+                this->manager->post(request, postData);
             return;
         }
         pushNotification("Password validation failed(should has lenght >= 8 and at least 1 digit)");
+    } else {
+
     }
 }
 
 void AuthDialog::pushNotification(QString message) {
     NotificationDialog dialog(message, this);
     dialog.exec();
+}
+
+void AuthDialog::signupReplyFinished(QNetworkReply *reply) {
+    QByteArray buffer = reply->readAll();
+    QJsonDocument jsonDoc(QJsonDocument::fromJson(buffer));
+    QJsonObject jsonReply = jsonDoc.object();
+    if (jsonReply["result"] != 0) {
+        pushNotification(jsonReply["error"].toString());
+    } else {
+        settings.setValue("token", jsonReply["token"].toString());
+        this->accept();
+    }
 }
 
 void AuthDialog::on_signUpCheckBox_stateChanged(int arg1)
--- authdialog.h
+++ authdialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QSettings>
+#include <QNetworkAccessManager>
 
 namespace Ui {
 class AuthDialog;
@@ -23,11 +24,15 @@ private slots:
 
     void on_AuthDialog_finished(int result);
 
+    void signupReplyFinished(QNetworkReply *networkReply);
+
 private:
     Ui::AuthDialog *ui;
     bool is_signup=false;
     QSettings settings;
     void pushNotification(QString message);
+    QNetworkAccessManager* manager = nullptr;
+    QString baseUrl = "http://127.0.0.1:8000/";
 };
 
 #endif // AUTHDIALOG_H
